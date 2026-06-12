a8064a5ecd967670b7c41927a4728c97db7f49d4
Aakash Kishan C K <aakash.ck@experionglobal.local>
2026-06-12 13:16:00 +0530

    fixed csa

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.database import engine
 from app.routes.user import router as user_router
 from mangum import Mangum
 #Base.metadata.create_all(bind=engine)
+from fastapi import Request
 
 app = FastAPI(title="User Management API")
 
@@ -14,6 +15,17 @@ def home():
     return {
         "message": "User Management API Running"
     }
+
+@app.middleware("http")
+async def log_request(request: Request, call_next):
+    print("PATH RECEIVED:", request.url.path)
+
+    response = await call_next(request)
+
+    print("STATUS:", response.status_code)
+
+    return response
+
 
 
 print("ROUTES:")
